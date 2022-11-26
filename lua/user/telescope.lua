4f193c258960e3c7e92650d6488ab53e642268be
aunknownwhoami <aunknown.whoami@gmail.com>
2022-11-26 21:09:56 +0530

    new_cfg

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -106,7 +106,7 @@ telescope.setup {
 
       n = {
         ["<esc>"] = actions.close,
-        ["<CR>"] = actions.select_default,
+        ["l"] = actions.select_default,
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
         ["<C-t>"] = actions.select_tab,
